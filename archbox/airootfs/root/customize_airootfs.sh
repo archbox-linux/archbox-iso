4a77ff3c29937c721cc336c55cc755d7bef8c69d
Alexander Glumoff <glumoff@gmail.com>
2016-01-24 03:55:13 +0200

    autologin with slim. added skeleton for user profile with configs

--- archbox/airootfs/root/customize_airootfs.sh
+++ archbox/airootfs/root/customize_airootfs.sh
@@ -11,6 +11,10 @@ usermod -s /usr/bin/zsh root
 cp -aT /etc/skel/ /root/
 chmod 700 /root
 
+USER=archbox
+
+! id $USER && useradd -m -p "" -g users -G "adm,audio,floppy,log,network,rfkill,scanner,storage,optical,power,wheel" -s /usr/bin/zsh $USER
+
 sed -i 's/#\(PermitRootLogin \).\+/\1yes/' /etc/ssh/sshd_config
 sed -i "s/#Server/Server/g" /etc/pacman.d/mirrorlist
 sed -i 's/#\(Storage=\)auto/\1volatile/' /etc/systemd/journald.conf
