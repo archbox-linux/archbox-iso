de184cbfce1f8b2c34064b9e84b998e5914d0292
Вочков Олександр Володимирович <vochkov@delta.uspa.gov.ua>
2016-01-31 00:30:50 +0200

    working on auto building customrepo

--- scripts/build-customrepo.sh
+++ scripts/build-customrepo.sh
@@ -9,4 +9,41 @@ fi
 
 cd "$REPO_DIR/build"
 
-cat ./packages-local.lst | xargs yaourt -G
+#download packages from aur
+cat $REPO_DIR/packages-aur.lst | xargs yaourt -G
+
+echo "copying theme"
+cp -r "$ROOT_DIR/artwork/themes/numix-themes-hreen" .
+
+#check devtools and install it (if needed) to build in clean chroot
+
+if [ ! -d "$REPO_DIR/chroot" ]; then
+  mkdir "$REPO_DIR/chroot"
+fi
+
+while read srcDir; do
+    echo "Package $srcDir"
+    if [ -d $srcDir ] && [ -f "$srcDir/PKGBUILD" ] ; then
+      echo "Enter to $srcDir"
+      cd "$srcDir"
+      #build package for 32bit arch
+      extra-i686-build -r $REPO_DIR/chroot
+      echo "Finished building 32 bit package"
+      #build package for 64bit arch
+#      extra-x86_64-build -r $REPO_DIR/chroot
+      
+      echo "Return to build dir"
+      cd "$REPO_DIR/build"
+    fi
+done <"$REPO_DIR/packages-aur.lst"
+
+#for srcDir in *; do
+#    if [ -d $srcDir ] && [ -f "$srcDir/PKGBUILD" ] ; then
+#      cd "$srcDir"
+#      #build package for 32bit arch
+#      extra-i686-build -r $REPO_DIR/chroot
+#      #build package for 64bit arch
+##      extra-x86_64-build -r $REPO_DIR/chroot
+#    fi
+#done
+
