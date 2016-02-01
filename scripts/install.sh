7e45a2c265968ccad8945d77750dc49c83a1f281
Alexander Glumoff <glumoff@gmail.com>
2016-02-02 01:24:10 +0200

    keep working

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/bash
 
+DEVICES=`lsblk -lno NAME,FSTYPE,SIZE,LABEL | egrep "(ext(2|3|4)|reiserfs|btrfs|reiser4)"`
 
 cnt=0
-for dev in `lsblk -fnl | egrep "(ext(2|3|4)|reiserfs|btrfs|reiser4)" | grep -o ^[^[:space:]]*`; do
+#for dev in `lsblk -lo NAME,FSTYPE,SIZE,LABEL | egrep "(ext(2|3|4)|reiserfs|btrfs|reiser4)" | grep -o ^[^[:space:]]*`; do
+while read -r dev; do
     (( cnt++ ))
-    dlgParams+=" ${cnt} ${dev} off"
-done
+    dlgParams+=( "$cnt" "$dev" "off" )
+    #echo "${cnt} \"${dev}\" off";
+done <<< "$DEVICES"
+
+#echo "${dlgParams[@]}"
 
 if [ $cnt -ne 0 ]; then
-    dialog --backtitle "Choose device for new root" --radiolist "Available devices:" 15 40 10  $dlgParams
+    dialog --backtitle "Choose device for new root" --radiolist "Available devices:" 15 40 10  "${dlgParams[@]}"
 fi
